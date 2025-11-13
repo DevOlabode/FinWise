a8d1c8440e303eb3e743f302fc4c93f2b13c1b45
Samuel Olabode <Solabode499@gmail.com>
2025-11-12 17:10:06 -0700

    `Refactored auth routes to use user controller`

--- routes/auth.js
+++ routes/auth.js
@@ -1,37 +1,11 @@
 const express  = require('express');
 const router = express.Router();
-const passport = require('passport');
 
-const User = require('../models/user');
+const controller = require('../controllers/user');
 
-const ExpressError  = require('../utils/expressError');
-const catchAsync  = require('../utils/catchAsync');
+router.post('/register', controller.register);
 
-router.post('/register', catchAsync(async(req, res)=>{
-    const {username, email, password} = req.body;
-    const user = await User.register(new User({ username, email }), password);
-    await user.save();
-
-    res.status(201).json({msg : 'User registered successfully'});
-}));
-
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-    if (err) {
-        return next(err);
-    }
-    if (!user) {
-        return res.status(401).json({ msg: info.message || 'Login failed' });
-    }
-
-    req.logIn(user, (err) => {
-        if (err) {
-            return next(err)
-        }
-            return res.status(200).json({ msg: 'Logged in successfully' });
-        });
-    })(req, res, next);
-});
+router.post('/login', controller.login);
 
 const isLoggedIn = function(req, res, next){
     if(!req.isAuthenticated()){
