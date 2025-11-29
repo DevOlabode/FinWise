8a760e5f36076a489f42ba66ecde09c2be448ffb
Samuel Olabode <Solabode499@gmail.com>
2025-11-29 15:32:39 -0700

    `Removed isLoggedIn middleware from routes`

--- routes/main.js
+++ routes/main.js
@@ -5,17 +5,17 @@ const controller = require('../controllers/main');
 const { isLoggedIn } = require('../middleware');
 const catchAsync = require('../utils/catchAsync')
 
-router.get('/inflation', isLoggedIn,  catchAsync(controller.getInflationData));
+router.get('/inflation',  catchAsync(controller.getInflationData));
 
 router.get('/stock', catchAsync(controller.getStockData));
 
-router.get('/news', isLoggedIn, catchAsync(controller.getNewsArticles));
+router.get('/news', catchAsync(controller.getNewsArticles));
 
 router.get('/worldbank-docs', isLoggedIn, catchAsync(controller.getWorldBankDocs));
 
-router.get('/gdp', isLoggedIn, catchAsync(controller.getGDP));
+router.get('/gdp', catchAsync(controller.getGDP));
 
-router.get('/labour-market', isLoggedIn, catchAsync(controller.labourMarketData));
+router.get('/labour-market', catchAsync(controller.labourMarketData));
 
 router.get('/monetary', catchAsync(controller.monetary));
 
